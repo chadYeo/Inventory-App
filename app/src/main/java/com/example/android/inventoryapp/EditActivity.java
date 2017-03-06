fdd576171a1fc240f0a5a122a5454d4478c24cfc
chadyeo <chad.yeo.cy@gmail.com>
2017-03-06 00:27:39 -0500

    Added Up Navigation in EditActivity

--- app/src/main/java/com/example/android/inventoryapp/EditActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.inventoryapp;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -16,5 +18,15 @@ public class EditActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
