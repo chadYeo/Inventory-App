1edda01452f7c2c8ec2b3298b7a0103b95f6b4f2
chadyeo <chad.yeo.cy@gmail.com>
2017-03-05 23:57:20 -0500

    Added FloatingActionButton

---
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.inventoryapp;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do nothing for now
+            }
+        });
+    }
+}
